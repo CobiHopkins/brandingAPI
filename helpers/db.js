97c3db6e9ea850f4f28e4d2d640896015dba855d
Cobi Hopkins <cobihopkins@yahoo.com>
2025-11-04 14:34:00 +0000

    Added new run_query function to navigate to connection pooling.

--- helpers/db.js
+++ helpers/db.js
@@ -2,14 +2,39 @@ const mysql = require('promise-mysql');
 const { databaseInfo } = require('../config');
 const { logger } = require('./logger');
 
+const pool = mysql.createPool({
+            host: DB_HOST,
+            user: DB_USER,
+            password: DB_PASSWORD,
+            database: DB_NAME,
+            port: DB_PORT,
+            connectionLimit: 10
+        });
+
+// needs testing and comparison for performance
 exports.run_query = async (query, values) => {
     try {
-        const conn = await mysql.createConnection(databaseInfo);
-        let data = await conn.query(query, values);
-        await conn.end();
+      
+        const conn = await pool.getConnection();
+        let [data] = await conn.query(query, values);
         return data;
+
       } catch (error) {
         logger.error("Database query error", error);
-        return error;
+        throw error;
+      } finally {
+        if (conn) conn.release();
       }
 }
+
+// exports.run_query = async (query, values) => {
+//     try {
+//         const conn = await mysql.createConnection(databaseInfo);
+//         let data = await conn.query(query, values);
+//         await conn.end();
+//         return data;
+//       } catch (error) {
+//         logger.error("Database query error", error);
+//         return error;
+//       }
+// }
